25156ff5df9049901d7bbe58dbd2f3fd24f0cd5a
kirby0113 <2000ryo0113@ezweb.ne.jp>
2021-07-28 19:05:34 +0900

    Checkboxs.jsをApp.jsにimport

--- resas-app/src/App.js
+++ resas-app/src/App.js
@@ -1,11 +1,14 @@
 
 import './App.css';
-import Header from "./components/Header"
+import Header from "./components/Header";
+import CheckBoxs from "./components/CheckBoxs";
+
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <CheckBoxs></CheckBoxs>
       Hello World.
     </div>
   );
