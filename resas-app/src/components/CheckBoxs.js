34b99eadd61efced888ec448681a64ab67265b24
kirby0113 <2000ryo0113@ezweb.ne.jp>
2021-07-28 19:08:17 +0900

    componentDidMount内にAPIを取得するコードを記述

--- resas-app/src/components/CheckBoxs.js
+++ resas-app/src/components/CheckBoxs.js
@@ -8,3 +8,16 @@ export default class CheckBoxs extends React.Component{
             prefs:[]
         }
     }
+//APIから都道府県名の取得
+componentDidMount(){
+    try{
+       axios.get(
+        'https://opendata.resas-portal.go.jp/api/v1/prefectures',{ headers: {"X-API-KEY":"QdcBeaEZsZeDqYRyHdNIpt4iU26GTa8ERHG1tdXh"}}
+      ).then(res => {
+          const prefs = res.data.result;
+          console.log(prefs);
+          this.setState({prefs});
+      });
+    }catch(error){
+      console.log("error:",error);
+    }
