404a456b8619d2f1ff861bf565f20759a4be06bc
kirby0113 <2000ryo0113@ezweb.ne.jp>
2021-07-29 05:36:33 +0900

    コンフリクトの解消

--- resas-app/src/App.js
+++ resas-app/src/App.js
@@ -2,23 +2,16 @@
 import './App.css';
 import Header from "./components/Header";
 import CheckBoxs from "./components/CheckBoxs";
-<<<<<<< HEAD
 import Graph from "./components/Graph";
-=======
->>>>>>> develop
 
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
-<<<<<<< HEAD
       <h2>都道府県</h2>
       <CheckBoxs></CheckBoxs>
       <Graph></Graph>
-=======
-      <CheckBoxs></CheckBoxs>
->>>>>>> develop
       Hello World.
     </div>
   );
