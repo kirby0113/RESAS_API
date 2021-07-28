004cb002b323d628acf51a9a70b21b03b1f227c9
kirby0113 <2000ryo0113@ezweb.ne.jp>
2021-07-28 19:07:17 +0900

    CheckBoxsクラスとstateを作成

--- resas-app/src/components/CheckBoxs.js
+++ resas-app/src/components/CheckBoxs.js
@@ -1,2 +1,10 @@
 import React from "react";
 import axios from "axios";
+export default class CheckBoxs extends React.Component{
+
+    constructor(props){
+        super(props)
+        this.state = {
+            prefs:[]
+        }
+    }
