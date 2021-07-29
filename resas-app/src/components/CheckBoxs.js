95b079bfaabc21abf11a6a1f7c3bd9efad97747a
kirby0113 <2000ryo0113@ezweb.ne.jp>
2021-07-30 02:43:10 +0900

    CheckBoxs.jsをクラスコンポーネントではなく関数コンポーネントに変更

--- resas-app/src/components/CheckBoxs.js
+++ resas-app/src/components/CheckBoxs.js
@@ -2,17 +2,10 @@ import React from 'react';
 
 import './CheckBoxs.css';
 
-export default class CheckBoxs extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      prefs: [],
-    };
-  }
-  render() {
+const CheckBoxs = (props) => {
     return (
       <div className="CheckBoxs">
-        {this.state.prefs.map((pref) => (
+        {props.prefs.map((pref) => (
           <div key={pref.prefName}>
             <label htmlFor={pref.prefName}>{pref.prefName}</label>
             <input
@@ -20,10 +13,11 @@ export default class CheckBoxs extends React.Component {
               name="prefName"
               value={pref.prefCode}
               id={pref.prefName}
-              onChange={this.props.onChangeCheck}></input>
+              onChange={props.onChangeCheck}></input>
           </div>
         ))}
       </div>
     );
-  }
 }
+
+export default CheckBoxs;
