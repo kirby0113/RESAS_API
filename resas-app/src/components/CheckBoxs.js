694da8fc586a713dc706c72db189f93ab24c3dba
kirby0113 <2000ryo0113@ezweb.ne.jp>
2021-08-01 06:13:26 +0900

    CheckBoxs.jsの不要なimportの削除・prettier実行

--- resas-app/src/components/CheckBoxs.js
+++ resas-app/src/components/CheckBoxs.js
@@ -1,23 +1,21 @@
-import React from 'react';
-
 import './CheckBoxs.css';
 
 const CheckBoxs = (props) => {
-    return (
-      <div className="CheckBoxs">
-        {props.prefs.map((pref) => (
-          <div key={pref.prefName}>
-            <label htmlFor={pref.prefName}>{pref.prefName}</label>
-            <input
-              type="checkbox"
-              name="prefName"
-              value={pref.prefCode}
-              id={pref.prefName}
-              onChange={props.onChangeCheck}></input>
-          </div>
-        ))}
-      </div>
-    );
-}
+  return (
+    <div className="CheckBoxs">
+      {props.prefs.map((pref) => (
+        <div key={pref.prefName}>
+          <label htmlFor={pref.prefName}>{pref.prefName}</label>
+          <input
+            type="checkbox"
+            name="prefName"
+            value={pref.prefCode}
+            id={pref.prefName}
+            onChange={props.onChangeCheck}></input>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default CheckBoxs;
