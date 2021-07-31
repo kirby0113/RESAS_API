f2f33f139be409cf24015c9cbd413334a83a592b
kirby0113 <2000ryo0113@ezweb.ne.jp>
2021-08-01 06:40:48 +0900

    Graph.js内のコード修正
    ・グラフ内の各線の色をランダムに設定
    ・一先ず１９９５年から２０２０年までのデータを５年おきに表示できるようにプロパティと条件式を設定

--- resas-app/src/components/Graph.js
+++ resas-app/src/components/Graph.js
@@ -1,37 +1,59 @@
+import {useEffect, useState} from 'react';
+
 import {Line} from 'react-chartjs-2';
 
-const data = {
-  labels: ['1995年', '2000年', '2005年', '2010年', '2015年', '2020年'],
-  datasets: [
-    {
-      label: 'Test Graph',
-      fill: false,
-      lineTension: 0.1,
-      backgroundColor: 'rgba(0,0,0)',
-      borderColor: 'rgba(0,0,0)',
-      borderCapStyle: 'round',
-      borderDash: [],
-      borderDashOffset: 0.0,
-      borderJoinStyle: 'square',
-      pointBorderColor: 'rgba(255,0,0)',
-      pointBackgroundColor: '#eee',
-      pointBorderWidth: 10,
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: 'rgba(0,255,0)', //各ポイントにカーソルが当たった時のポイントの背景色
-      pointHoverBorderColor: 'rgba(0,0,255)', //各ポイントにカーソルが当たった時のポインタのボーダー色
-      pointHoverBoderWidth: 1,
-      pointRadius: 1,
-      pointHitRadius: 10,
-      data: [1, 10, 100, 50, 60, 80],
-    },
-  ],
-};
+function Graph(props) {
+  const [graphState, setGraphState] = useState();
+  useEffect(() => {
+    let datasets = props.graphDatas.map((graphData) => {
+      let color = `rgba(${Math.floor(Math.random() * 255)},${Math.floor(
+        Math.random() * 255
+      )},${Math.floor(Math.random() * 255)})`;
+      console.log(graphData);
+      const validdata = graphData.data
+        .map((data) => {
+          if (data.year >= 1995 && data.year <= 2020) {
+            return data.value;
+          } else {
+            return;
+          }
+        })
+        .filter((data) => data);
 
-function Graph() {
+      return {
+        label: graphData.key,
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: color,
+        borderColor: color,
+        borderCapStyle: 'round',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'square',
+        pointBorderColor: 'rgba(255,0,0)',
+        pointBackgroundColor: '#eee',
+        pointBorderWidth: 10,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: 'rgba(0,255,0)', //各ポイントにカーソルが当たった時のポイントの背景色
+        pointHoverBorderColor: 'rgba(0,0,255)', //各ポイントにカーソルが当たった時のポインタのボーダー色
+        pointHoverBoderWidth: 1,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: validdata,
+      };
+    });
+    const data = {
+      labels: ['1995年', '2000年', '2005年', '2010年', '2015年', '2020年'],
+      datasets: datasets,
+    };
+    console.log(data);
+    setGraphState(data);
+  }, [props.graphDatas]);
+
   return (
     <div>
-      <h2>Test Graph</h2>
-      <Line data={data} height={100} />
+      <h2>各都道府県の人口推移</h2>
+      <Line data={graphState} height={70} />
     </div>
   );
 }
